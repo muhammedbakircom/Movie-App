dd16f649e56aa543d5d790b9451ba6b14c4a5b80
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-09-21 11:36:19 +0300

    addComponents&addReactRouter

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import "./lib/fontawesome/css/all.min.css"
 import Header from './Components/Header';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Watchlist from './Components/Watchlist';
+import Watched from './Components/Watched';
+import Add from './Components/Add';
 
 function App() {
   return (
-    <div className='App'>
+    <Router>
       <Header />
-    </div>
-
+      <Routes>
+        <Route path="/" element={<Watchlist />} />
+        <Route path="/watched" element={<Watched />} />
+        <Route path="/add" element={<Add />} />
+      </Routes>
+    </Router>
     );
 }
 
