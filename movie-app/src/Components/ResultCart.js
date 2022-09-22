8bbe152f1e8918750ded7c0089046e6df151b392
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-09-22 13:16:45 +0300

    getmovieimagecart

---
+++ movie-app/src/Components/ResultCart.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ResultCart({result}) {
+    console.log(result);
+
+  return (
+    <div className='result-card'>
+      <div className='poster-wrapper'>
+        <img 
+        src={`https://image.tmdb.org/t/p/w200/${result.poster_path}`} 
+        alt={`${result.poster_path}`}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ResultCart
