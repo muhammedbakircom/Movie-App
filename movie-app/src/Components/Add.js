ce816a2b0d28d2c0302f82f07ad1eb32b2d22a82
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-09-22 13:17:16 +0300

    getApi&checkApi

--- movie-app/src/Components/Add.js
+++ movie-app/src/Components/Add.js
@@ -1,19 +1,27 @@
 /* eslint-disable no-unused-expressions */
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
+import ResultCart from "./ResultCart";
 
 function Add() {
-    const [query,setQuery] = useState("");
-    console.log(query);
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
 
   const onChange = (e) => {
-    setQuery(e.target.value)
+    setQuery(e.target.value);
     fetch(`
     https://api.themoviedb.org/3/search/movie?api_key=d44788546017a8f852ad299cb3990607&
-    language=en-US&page=1&include_adult=false&query=${e.target.value}`)
-    .then((response) => response.json()
-    .then((data) => console.log(data)));
-  }
+    language=en-US&page=1&include_adult=false&query=${e.target.value}`).then(
+      (response) =>
+        response.json().then((data) => {
+          if (!data.errors) {
+            setResults(data.results);
+          } else {
+            setResults([]);
+          }
+        })
+    );
+  };
 
   return (
     <div className="add-page">
@@ -30,12 +38,22 @@ function Add() {
             </h2>
           </div>
           <div className="input-wrapper">
-            <input 
-            type="text"  
-            value={query}
-            onChange={onChange}    
-            placeholder="Film,dizi ara.." />
+            <input
+              type="text"
+              value={query}
+              onChange={onChange}
+              placeholder="Film,dizi ara.."
+            />
           </div>
+          {results.length > 0 && (
+            <ul className="results">
+              {results.map((result, index) => (
+                <li key={index}>
+                  <ResultCart result={result} />
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </div>
