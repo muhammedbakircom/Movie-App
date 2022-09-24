6a7645c2f9bb7400e01798161bd1e1a630432c09
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-09-24 13:15:07 +0300

    useReducer&initialState

--- movie-app/src/Context/GlobalState.js
+++ movie-app/src/Context/GlobalState.js
@@ -1,10 +1,18 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
 
 // create context
 export const GlobalContext = createContext();
 
+const initialState = {
+    watchlist : [],
+    watched : []
+}
+
 //create provider components
 export const GlobalProvider = (props) => {
+    const[state,dispatch] = useReducer(AppReducer,initialState);
+    console.log(state);
   return (
     <GlobalContext.Provider value={{ barbaros: "developer" }}>
       {props.children}
