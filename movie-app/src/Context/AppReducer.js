10dbd0364855eac1928930cf83d080e45413e593
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-09-25 14:12:41 +0300

    Components-AppReducer

--- movie-app/src/Context/AppReducer.js
+++ movie-app/src/Context/AppReducer.js
@@ -1,1 +1,14 @@
-export default (state,action) => {};
+const foo = (state,action) => {
+
+    switch(action.type){
+        case "ADD_MOVİE_TO_WATCHLIST":
+            return{
+                ...state,
+                watchlist:[...state.watchlist,action.payload]
+            };
+    default:
+        return state;
+  }
+};
+
+export default foo
