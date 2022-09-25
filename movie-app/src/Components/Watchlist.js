88ca47860441d7e6f39c732cba55be1bf2fe6ebc
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-09-25 21:20:39 +0300

    displayWatchlist

--- movie-app/src/Components/Watchlist.js
+++ movie-app/src/Components/Watchlist.js
@@ -1,13 +1,25 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../Context/GlobalState'
+import MovieCard from './MovieCard';
 
 function Watchlist() {
   const {watchlist} = useContext(GlobalContext);
   return (
-    <div>
-      {watchlist.map((result)=>(
-        <h1>{result.title}</h1>
-      ))}
+    <div className='movie-page'>
+      <div className='container'>
+        <div className='header'>
+          <div className='heading'>
+        <h1>İzlenecek Filmler</h1>
+          </div>
+          {watchlist.length > 0 &&(
+            <div className='movie-grid'>
+              {watchlist.map((movie,index)=> (
+                <MovieCard key={index} movie={movie} />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
