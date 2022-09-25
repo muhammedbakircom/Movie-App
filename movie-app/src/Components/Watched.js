d1cea528c0032c32cfbdcdf74a98e257361454c0
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-09-25 21:20:45 +0300

    displayWatched

--- movie-app/src/Components/Watched.js
+++ movie-app/src/Components/Watched.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../Context/GlobalState'
+import MovieCard from './MovieCard';
 
 function Watched() {
+  const {watched} = useContext(GlobalContext);
   return (
-    <div>
-      
+    <div className='movie-page'>
+      <div className='container'>
+        <div className='header'>
+          <div className='heading'>
+        <h1>İzlenecek Filmler</h1>
+          </div>
+          {watched.length > 0 &&(
+            <div className='movie-grid'>
+              {watched.map((movie,index)=> (
+                <MovieCard key={index} movie={movie} />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
