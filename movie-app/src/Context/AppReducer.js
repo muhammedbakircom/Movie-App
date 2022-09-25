e114f8245526d051746f3dd3011577e921770985
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-09-25 21:19:39 +0300

    globalState-addReducer

--- movie-app/src/Context/AppReducer.js
+++ movie-app/src/Context/AppReducer.js
@@ -6,6 +6,21 @@ const foo = (state,action) => {
                 ...state,
                 watchlist:[...state.watchlist,action.payload]
             };
+
+        case "REMOVE_MOVİE_TO_WATCHLIST":
+            return{
+                ...state,
+                watchlist: state.watchlist.filter(
+                    (movie) => movie.id !== action.payload
+                    ),
+            };
+            case "ADD_MOVİE_TO_WATCHED":
+                return{
+                    ...state,
+                    watchlist:state.watchlist.filter((movie) => movie.id !== action.payload.id
+                    ),
+                    watched:[...state.watched, action.payload],
+                };
     default:
         return state;
   }
