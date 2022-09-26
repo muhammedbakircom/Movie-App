4a65ab5439876d897f957a13c4de234e596d2021
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-09-26 11:03:38 +0300

    addContextApi&reducer

--- movie-app/src/Context/AppReducer.js
+++ movie-app/src/Context/AppReducer.js
@@ -17,10 +17,27 @@ const foo = (state,action) => {
             case "ADD_MOVİE_TO_WATCHED":
                 return{
                     ...state,
-                    watchlist:state.watchlist.filter((movie) => movie.id !== action.payload.id
+                    watchlist:state.watchlist.filter(
+                        (movie) => movie.id !== action.payload.id
                     ),
                     watched:[...state.watched, action.payload],
                 };
+                case "MOVE_TO_WATCHED":
+                    return{
+                        ...state,
+                        watched:state.watched.filter(
+                            (movie) => movie.id !== action.payload.id
+                        ),
+                        watchlist:[...state.watchlist, action.payload],
+                    };
+                    case "REMOVE_MOVIE_TO_WATCHED":
+                        return{
+                            ...state,
+                            watched:state.watched.filter(
+                                (movie) => movie.id !== action.payload
+                            ),
+                        };
+    
     default:
         return state;
   }
