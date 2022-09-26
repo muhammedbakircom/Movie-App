79447b39ec4a638cb5d051056d39e1dbd8d34cfe
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-09-26 11:31:58 +0300

    displayMovieControl&onClick

--- movie-app/src/Components/MovieControls.js
+++ movie-app/src/Components/MovieControls.js
@@ -1,19 +1,41 @@
-import React, { useContext } from 'react'
-import { GlobalContext } from '../Context/GlobalState'
+import React, { useContext } from "react";
+import { GlobalContext } from "../Context/GlobalState";
 
-function MovieControls({movie}) {
-  const {removeMovieToWatchlist , addMovieToWatched} = useContext(GlobalContext)
+function MovieControls({ movie, type }) {
+  const { removeMovieToWatchlist, addMovieToWatched , moveToWatchlist , removeMovieWatched} =
+    useContext(GlobalContext);
   return (
-    <div className='inner-card-controls'>
-        <button className='ctrl-btn' onClick={() => addMovieToWatched(movie) }>
-            <i className='fa-fw far fa-eye'></i>
-        </button>
-        <button className='ctrl-btn' onClick={() => removeMovieToWatchlist(movie.id)}>
-            <i className='fa-fw fa fa-times'></i>
-        </button>
-      
+    <div className="inner-card-controls">
+      {type === "watchlist" && (
+        <>
+          <button className="ctrl-btn" onClick={() => addMovieToWatched(movie)}>
+            <i className="fa-fw far fa-eye"></i>
+          </button>
+          <button
+            className="ctrl-btn"
+            onClick={() => removeMovieWatched(movie.id)}
+          >
+            <i className="fa-fw fa fa-times"></i>
+          </button>
+        </>
+      )}
+
+{type === "watched" && (
+        <>
+          <button className="ctrl-btn" onClick={() => addMovieToWatched(movie)}>
+            <i className="fa-fw far fa-eye-slash" onClick={() => moveToWatchlist(movie)}></i>
+          </button>
+          <button
+            className="ctrl-btn"
+            onClick={() => removeMovieToWatchlist(movie.id)}
+          >
+            <i className="fa-fw fa fa-times"></i>
+          </button>
+        </>
+      )}
+
     </div>
-  )
+  );
 }
 
-export default MovieControls
+export default MovieControls;
